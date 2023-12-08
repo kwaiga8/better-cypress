471f06799cd0f36100d5dbe424322f463e3f49de
kwaiga8 <kwaiga8@gmail.com>
2023-12-08 09:17:03 +0100

    Adding scaffold for logout scenario

---
+++ cypress/e2e/logging/logout.cy.ts
@@ -0,0 +1,21 @@
+import { LoginPage } from '../../support/views/login/loginPage'
+import { MainShopPage } from '../../support/views/inventory/inventory'
+
+describe('User can successfully log out', () => {
+  const loginPage: LoginPage = new LoginPage()
+  const mainShopPage: MainShopPage = new MainShopPage()
+
+  beforeEach(() => {
+    loginPage.visit()
+    loginPage.form.passCorrectUsername()
+    loginPage.form.passCorrectPassword()
+    loginPage.form.submit()
+    mainShopPage.isCorrectlyLoaded()
+  })
+
+  it('is possible to log out', () => {
+   
+  })
+
+ 
+})
